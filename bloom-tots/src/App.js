ba93e44ff6bc39906ac7b6e0df2d9874eeba2942
nandinicloud <163637749+nandinicloud@users.noreply.github.com>
2026-05-22 02:41:03 +0530

    update

--- bloom-tots/src/App.js
+++ bloom-tots/src/App.js
@@ -1,31 +1,4 @@
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-import Home from "./components/Home";
-
+import Home from "./Home";
 function App() {
   return (
     <div>
@@ -34,4 +7,4 @@ function App() {
   );
 }
 
-export default App;
+export default App; 
